64e88f0d533061537a1c9adfb66143a691d0a703
Michael Law <michaellaw@gmx.com>
2020-11-28 15:22:50 -0500

    setup view engine and bodyparser for forms

--- server.js
+++ server.js
@@ -3,8 +3,18 @@ const app = express()
 const morgan = require('morgan')
 const host = '127.0.0.1'
 const port = 3000
+const bodyParser = require('body-parser')
+const { Liquid } = require('liquidjs')
+const engine = new Liquid({
+  root: ['.', './views/layouts'], // where the engine will look for .liquid files
+  extname: '.liquid' // now we don't have to write .liquid on every file
+})
+app.engine('liquid', engine.express())
+app.use(bodyParser.urlencoded({ extended: true })) // to handle form parsing
+app.set('views', './views') // set the directory where app will look for html/view files
+app.set('view engine', 'liquid') // set the view engine
 
-app.use(morgan('dev'))
+app.use(morgan('dev')) // server logging
 
 app.get('/', (req, res) => {
   res.send('<h1>Welcome to Eventlite</h1>')
@@ -16,6 +26,10 @@ app.get('/login', (req, res) => {
 
 app.get('/signup', (req, res) => {
   res.send('<h1>Signup</h1>')
+})
+
+app.get('/users/account', (req, res) => {
+  res.send('<h1>Account settings</h1>')
 })
 
 app.get('/events/all', (req, res) => {
