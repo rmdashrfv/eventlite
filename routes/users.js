d3f770d4f74e187a5e58e9c5c472f7ead1a13a1e
Michael Law <michaellaw@gmx.com>
2020-11-28 17:05:41 -0500

    Added all users route and showing how to get dynamic content in the views

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 const express = require('express') // instantiate express
+const { User } = require('../models')
 const router = express.Router() // create a router object
 
 router.get('/login', (req, res) => {
@@ -9,8 +10,28 @@ router.get('/users/new', (req, res) => {
   res.render('users/new')
 })
 
-router.get('/users/account', (req, res) => {
-  res.send('<h1>Account settings</h1>')
+router.post('/users/new', async (req, res) => {
+  console.log(req.body)
+  let email = req.body.email
+  let password = req.body.password
+  let user = User.build({email: email, password: password})
+  let userWasSaved = await user.save()
+  if (userWasSaved) {
+    res.render('users/account', {user: user})
+  } else {
+    console.log('Error', userWasSaved)
+    res.send({message: 'error'})
+  }
+})
+
+router.get('/users/account/:id', async (req, res) => {
+  let user = await User.findOne({where: {id: req.params.id}})
+  res.render('users/account', {user: user})
+})
+
+router.get('/users/all', async (req, res) => {
+  let allUsers = User.findAll()
+  res.render('users/all', {allUsers: allUsers})
 })
 
 module.exports = router
